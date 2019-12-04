d35cd836ccc51984d569be4bba0d966730907814
Robert Jernberg <robert.jernberg@devcode.se>
2019-12-04 21:50:15 +0100

    Add unit test support

--- AdventOfCode/Year2019/Solver2019_1_1.cs
+++ AdventOfCode/Year2019/Solver2019_1_1.cs
@@ -3,9 +3,9 @@ using System.Linq;
 
 namespace AdventOfCode.Year2019
 {
-    class Solver2019_1_1
+    static class Solver2019_1_1
     {
-        public string Solve(IInputResolver input)
+        public static string Solve(IInputResolver input)
             => input.AsEnumerable().Sum(s => int.Parse(s)/3 - 2).ToString();
     }
 }
---
+++ AdventOfCode/Year2019/Tests/Tester2019_1.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode.Utility;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Year2019.Tests
+{
+    public class Tester2019_1
+    {
+        [Theory]
+        [InlineData(2, "2")]
+        [InlineData(3, "2")]
+        [InlineData(4, "654")]
+        [InlineData(5, "33583")]
+        public void FirstProblem(int example, string expected)
+        {
+            Solver2019_1_1.Solve(new FileInputResolver(2019, 1, example)).Should().Be(expected);
+        }
+    }
+}
