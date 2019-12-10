3fab3d79b3c4be73f84d9598547ad206d9772c9b
Robert Jernberg <robert.jernberg@devcode.se>
2019-12-10 23:55:58 +0100

    Working generic intcode computer class
    First version is using strings to build the function sets.

--- AdventOfCode/Year2019/IntcodeComputer.cs
+++ AdventOfCode/Year2019/IntcodeComputer.cs
@@ -1,91 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AdventOfCode.Year2019
+﻿namespace AdventOfCode.Year2019
 {
     class IntcodeComputer
     {
-        private int pointer = 0;
         private readonly long[] instructions;
+        private readonly long[] _originalIns;
+
+        public int Pointer { get; private set; }
         public bool Halted { get; private set; }
         public bool NeedNewInput { get; private set; }
+        public bool ProvidedOutput { get; private set; }
         public long LastOutput { get; private set; }
+        public int RelativeBase { get; private set; }
         public long LastSetValue { get; private set; }
 
         public IntcodeComputer(long[] instructions)
         {
             this.instructions = instructions;
+
+            _originalIns = new long[instructions.Length];
+            instructions.CopyTo(_originalIns, 0);
         }
 
-        public long Compute(long input)
+        public void Reset()
+        {
+            _originalIns.CopyTo(instructions, 0);
+            Pointer = 0;
+            Halted = false;
+            RelativeBase = 0;
+            NeedNewInput = false;
+            LastOutput = 0;
+            LastSetValue = 0;
+        }
+
+        public ProgramState RunProgram(long input)
+        {
+            while(!Halted)
+            {
+                Compute(input);
+                if (NeedNewInput) return ProgramState.NeedInput;
+                if (ProvidedOutput) return ProgramState.ProvidedOutput;
+            }
+            return ProgramState.Halted;
+        }
+
+        public void Compute(long input)
         {
             NeedNewInput = false;
-
-            var opcodeAndParams = instructions[pointer];
-            var opcode = (int)opcodeAndParams % 100;
-            var programInput = input;
-            long inp1 = 0;
-            long inp2 = 0;
-            long outp = 0;
-            long inv1 = 0;
-            long inv2 = 0;
-            int relativeBase = 0;
-            int opcodeParamCount = 0;
-            var temp = 0L;
+            ProvidedOutput = false;
+            long inp1, inp2, outp, inv1, inv2, temp;
+            inp1 = inp2 = outp = inv1 = inv2 = temp = 0L;
             int param1Type = 0, param2Type = 0, param3Type = 0;
 
-            if(opcode == 99)
+            var opcodeAndParams = instructions[Pointer];
+            var opcode = (int)opcodeAndParams % 100;
+
+            if (opcode == 99)
             {
                 Halted = true;
-                return -1;
+                return;
             }
 
-            if ("_xxxx"[opcode] == 'x') param1Type = (int)opcodeAndParams / 100 % 10;
-            if ("_xx__"[opcode] == 'x') param2Type = (int)opcodeAndParams / 1000 % 10;
-            if ("_xx__"[opcode] == 'x') param3Type = (int)opcodeAndParams / 10000 % 10;
-            if ("_xx_x"[opcode] == 'x') inp1 = instructions[pointer + 1];
-            if ("_xx__"[opcode] == 'x') inp2 = instructions[pointer + 2];
-            if ("_xx__"[opcode] == 'x') outp = instructions[pointer + 3];
-            if ("_xx_x"[opcode] == 'x') inv1 = GetInput(param1Type, inp1, instructions, relativeBase);
-            if ("_xx__"[opcode] == 'x') inv2 = GetInput(param2Type, inp2, instructions, relativeBase);
-            if ("_x___"[opcode] == 'x') temp = inv1 + inv2;
-            if ("__x__"[opcode] == 'x') temp = inv1 * inv2;
-            if ("____x"[opcode] == 'x') LastOutput = inv1;
-            if ("___x_"[opcode] == 'x') temp = programInput;
-            if ("_xx__"[opcode] == 'x') outp = GetOutP(param3Type, pointer + 3, instructions, relativeBase);
-            if ("___x_"[opcode] == 'x') outp = GetOutP(param1Type, pointer + 1, instructions, relativeBase);
-            if ("_xx__"[opcode] == 'x') instructions[outp] = temp;
-            if ("_xx__"[opcode] == 'x') opcodeParamCount += 4;
-            if ("___xx"[opcode] == 'x') opcodeParamCount += 2;
-            if ("___x_"[opcode] == 'x') NeedNewInput = true;
-
-            if ("_xxxx"[opcode] == 'x') pointer += opcodeParamCount;
+            // op:123456789
+            // Get parameter modes: 0 = position mode, 1 = immediate mode, 2 = relative mode.
+            if ("_xxxxxxxxx"[opcode] == 'x') param1Type = (int)opcodeAndParams / 100 % 10;
+            if ("_xx__xxxx_"[opcode] == 'x') param2Type = (int)opcodeAndParams / 1000 % 10;
+            if ("_xx____xx_"[opcode] == 'x') param3Type = (int)opcodeAndParams / 10000 % 10;
+            // Get parameter values. The actual value depends on the parameter mode.
+            if ("_xx_xxxxxx"[opcode] == 'x') inp1 = instructions[Pointer + 1];
+            if ("_xx__xxxx_"[opcode] == 'x') inp2 = instructions[Pointer + 2];
+            if ("_xx____xx_"[opcode] == 'x') outp = instructions[Pointer + 3];
+            // Get actual parameter values based on parameter mode.
+            if ("_xx_xxxxxx"[opcode] == 'x') inv1 = GetInput(param1Type, inp1, instructions, RelativeBase);
+            if ("_xx__xxxx_"[opcode] == 'x') inv2 = GetInput(param2Type, inp2, instructions, RelativeBase);
+            // opcode specific operations.
+            if ("_x________"[opcode] == 'x') temp = inv1 + inv2;
+            if ("__x_______"[opcode] == 'x') temp = inv1 * inv2;
+            if ("_______x__"[opcode] == 'x') temp = inv1 < inv2 ? 1 : 0;
+            if ("________x_"[opcode] == 'x') temp = inv1 == inv2 ? 1 : 0;
+            if ("___x______"[opcode] == 'x') { temp = input; NeedNewInput = true; }
+            if ("____x_____"[opcode] == 'x') { LastOutput = inv1; ProvidedOutput = true; }
+            if ("_________x"[opcode] == 'x') RelativeBase += (int)inv1;
+            // Get location pointer for output.
+            if ("___x______"[opcode] == 'x') outp = GetOutP(param1Type, Pointer + 1, instructions, RelativeBase);
+            if ("_xx____xx_"[opcode] == 'x') outp = GetOutP(param3Type, Pointer + 3, instructions, RelativeBase);
+            // Store value to memory location.
+            if ("_xxx___xx_"[opcode] == 'x') instructions[outp] = temp;
+            // Set program pointer.
+            if ("_xx____xx_"[opcode] == 'x') Pointer += 4;
+            if ("___xx____x"[opcode] == 'x') Pointer += 2;
+            if ("_____x____"[opcode] == 'x') Pointer = (inv1 != 0) ? (int)inv2 : Pointer + 3;
+            if ("______x___"[opcode] == 'x') Pointer = (inv1 == 0) ? (int)inv2 : Pointer + 3;
 
             LastSetValue = temp;
-            return temp;
+            return;
         }
 
         private static long GetInput(int type, long paramValue, long[] instructions, long relativeBase)
-        {
-            return type == 0 ? instructions[paramValue]
+            => type == 0 ? instructions[paramValue]
                             : type == 1 ? paramValue
                             : instructions[paramValue + relativeBase];
-        }
 
         private static long GetOutP(int type, int pointer, long[] instructions, int relativeBase)
-        {
-            if (type == 0)
-                return instructions[pointer];
-            else if (type == 2)
-            {
-                var t = instructions[pointer];
-                return t + relativeBase;
-            }
-            else
-            {
-                throw new Exception("Unexpected out type");
-            }
-        }
+         => type == 0 ? instructions[pointer]
+            : type == 2 ? instructions[pointer] + relativeBase
+            : throw new System.Exception("Parameter mode 1 not supported for output");
+    }
+
+    public enum ProgramState
+    {
+        Halted = -1,
+        None = 0,
+        NeedInput = 1,
+        ProvidedOutput = 2
     }
 }
--- AdventOfCode/Year2019/Tests/IntcodeComputerTests.cs
+++ AdventOfCode/Year2019/Tests/IntcodeComputerTests.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using Xunit;
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace AdventOfCode.Year2019.Tests
 {
@@ -18,7 +19,7 @@ namespace AdventOfCode.Year2019.Tests
                 .First().Split(',')
                 .Select(s => long.Parse(s)).ToArray();
             var sut = new IntcodeComputer(instructions);
-            var res = sut.Compute(0);
+            sut.Compute(0);
             sut.LastSetValue.Should().Be(expected);
         }
 
@@ -32,7 +33,7 @@ namespace AdventOfCode.Year2019.Tests
                 .First().Split(',')
                 .Select(s => long.Parse(s)).ToArray();
             var sut = new IntcodeComputer(instructions);
-            var res = sut.Compute(0);
+            sut.Compute(0);
             sut.LastSetValue.Should().Be(expected);
         }
 
@@ -46,7 +47,7 @@ namespace AdventOfCode.Year2019.Tests
                 .First().Split(',')
                 .Select(s => long.Parse(s)).ToArray();
             var sut = new IntcodeComputer(instructions);
-            var res = sut.Compute(0);
+            sut.Compute(0);
             sut.LastSetValue.Should().Be(expected);
         }
 
@@ -60,7 +61,7 @@ namespace AdventOfCode.Year2019.Tests
                 .First().Split(',')
                 .Select(s => long.Parse(s)).ToArray();
             var sut = new IntcodeComputer(instructions);
-            var res = sut.Compute(0);
+            sut.Compute(0);
             sut.LastSetValue.Should().Be(expected);
         }
 
@@ -74,7 +75,7 @@ namespace AdventOfCode.Year2019.Tests
                 .First().Split(',')
                 .Select(s => long.Parse(s)).ToArray();
             var sut = new IntcodeComputer(instructions);
-            var res = sut.Compute(0);
+            sut.Compute(0);
             sut.LastSetValue.Should().Be(expected);
         }
 
@@ -106,6 +107,134 @@ namespace AdventOfCode.Year2019.Tests
             var sut = new IntcodeComputer(instructions);
             sut.Compute(99);
             sut.LastOutput.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(2, "05,2,1,99")]
+        [InlineData(4, "1005,2,4,99,99")]
+        [InlineData(2005, "2005,1,0,99")]
+        [InlineData(3, "05,3,1,0,99")]
+        [InlineData(3, "105,0,4,99,99")]
+        [InlineData(3, "205,2,0,99")]
+        public void TestJumpIfTrue(int expected, string example)
+        {
+            var input = new MockInputResolver(new[] { example });
+            var instructions = input.AsEnumerable()
+                .First().Split(',')
+                .Select(s => long.Parse(s)).ToArray();
+            var sut = new IntcodeComputer(instructions);
+            sut.Compute(0);
+            sut.Pointer.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(3, "06,2,1,99")]
+        [InlineData(3, "1006,2,4,99,99")]
+        [InlineData(3, "2006,1,0,99")]
+        [InlineData(99, "06,3,4,0,99")]
+        [InlineData(4, "1106,0,4,99,99")]
+        [InlineData(206, "206,2,0,99")]
+        public void TestJumpIfTFalse(int expected, string example)
+        {
+            var input = new MockInputResolver(new[] { example });
+            var instructions = input.AsEnumerable()
+                .First().Split(',')
+                .Select(s => long.Parse(s)).ToArray();
+            var sut = new IntcodeComputer(instructions);
+            sut.Compute(0);
+            sut.Pointer.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(1, "07,1,2,3")]
+        [InlineData(0, "1107,3,-99,3")]
+        public void TestLessThan(long expected, string example)
+        {
+            var input = new MockInputResolver(new[] { example });
+            var instructions = input.AsEnumerable()
+                .First().Split(',')
+                .Select(s => long.Parse(s)).ToArray();
+            var sut = new IntcodeComputer(instructions);
+            sut.Compute(0);
+            sut.LastSetValue.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(1, "1108,56,56,3")]
+        [InlineData(0, "1108,3,-99,3")]
+        public void TestEqual(long expected, string example)
+        {
+            var input = new MockInputResolver(new[] { example });
+            var instructions = input.AsEnumerable()
+                .First().Split(',')
+                .Select(s => long.Parse(s)).ToArray();
+            var sut = new IntcodeComputer(instructions);
+            sut.Compute(0);
+            sut.LastSetValue.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(9, "1109,9,99")]
+        [InlineData(-9, "1109,-9,99")]
+        public void TestAdjustRelativeBAse(int expected, string example)
+        {
+            var input = new MockInputResolver(new[] { example });
+            var instructions = input.AsEnumerable()
+                .First().Split(',')
+                .Select(s => long.Parse(s)).ToArray();
+            var sut = new IntcodeComputer(instructions);
+            sut.Compute(0);
+            sut.RelativeBase.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(new long[] { 109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99 },
+            "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99")]
+        [InlineData(new [] {1219070632396864 }, "1102,34915192,34915192,7,4,7,99,0")]
+        [InlineData(new [] {1125899906842624 }, "104,1125899906842624,99")]
+        public void Day9ProblemExamples(long[] expected, string example)
+        {
+            var res = RunComputation(new MockInputResolver(new[] { example }), 1);
+
+            res.Should().Equal(expected);
+        }
+
+        [Fact]
+        public void FirstProblemMyInput()
+        {
+            var res = RunComputation(new FileInputResolver(2019, 9), 1);
+            res.Should().Equal(new long[] { 2171728567 });
+        }
+
+        [Fact]
+        public void SecondProblemMyInput()
+        {
+            var res = RunComputation(new FileInputResolver(2019, 9), 2);
+            res.Should().Equal(new long[] { 49815 });
+        }
+
+        private List<long> RunComputation(IInputResolver input, long startData)
+        {
+            var instructions = new long[10_000];
+            var inData = input.AsEnumerable()
+                .First().Split(',')
+                .Select(s => long.Parse(s)).ToArray();
+            inData.CopyTo(instructions, 0);
+
+            var sut = new IntcodeComputer(instructions);
+
+            var outp = new List<long>();
+            ProgramState state = ProgramState.None;
+            while (state != ProgramState.Halted)
+            {
+                state = sut.RunProgram(startData);
+                if (state == ProgramState.ProvidedOutput)
+                {
+                    outp.Add(sut.LastOutput);
+                }
+            }
+
+            return outp;
         }
     }
 }
