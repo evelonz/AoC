f26e2bd5a24a43814d636c1c4706b58d9edd4b66
Robert Jernberg <robert.jernberg@devcode.se>
2019-12-19 20:49:56 +0100

    Solve 2019 day 19

--- AdventOfCode/Year2019/Solver2019_19_1.cs
+++ AdventOfCode/Year2019/Solver2019_19_1.cs
@@ -1,27 +1,12 @@
 ﻿using AdventOfCode.Utility;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Year2019
 {
     static class Solver2019_19_1
     {
-
-        public static string Solve(IInputResolver input, bool print = false)
-        {
-            var res = GetMap(input);
-            return res.ToString();
-        }
-
-        //public static string SolveSecond(IInputResolver input, bool print = false)
-        //{
-        //    var (map, _, end) = GetMap(input, print);
-        //    var steps = Bfs(map, end);
-        //    return steps.ToString();
-        //}
-
-        private static string GetMap(IInputResolver input)
+        public static string Solve(IInputResolver input)
         {
             var instructions = new long[10_000];
             var inData = input.AsEnumerable()
@@ -29,27 +14,24 @@ namespace AdventOfCode.Year2019
                 .Select(s => long.Parse(s)).ToArray();
             inData.CopyTo(instructions, 0);
 
-            
-
-            long response = 0; // 0 = wall, 1 moved, 2 found oxygen system.
+            long response = 0; // 1 in field, 0 outside of field.
             ProgramState state = ProgramState.None;
-            int size = 130;
-            int yOffset = 1100;
+            int size = 50;
             int y = 0;
-            int xOffset = 650;
             int x = 0;
+            // Offsets can be used to draw other parts of the map.
+            int yOffset = 0;
+            int xOffset = 0;
 
             var map = new int[size][];
             var row = new int[size];
+            var sut = new IntcodeComputer(instructions);
+
             while (state != ProgramState.Halted)
             {
-                inData.CopyTo(instructions, 0);
-                var sut = new IntcodeComputer(instructions);
-                state = sut.RunProgram(null);
                 state = sut.RunProgram(x+xOffset);
                 state = sut.RunProgram(y+yOffset);
 
-                // Save the display values based on output.
                 if (state == ProgramState.ProvidedOutput)
                 {
                     response = sut.LastOutput;
@@ -64,7 +46,7 @@ namespace AdventOfCode.Year2019
                         break;
                 }
 
-
+                sut.Reset();
             }
 
             int sum = 0;
@@ -74,10 +56,6 @@ namespace AdventOfCode.Year2019
                 for (int xx = 0; xx < roww.Length; xx++)
                 {
                     var v = roww[xx];
-                    //if(yy+yOffset >= 673 && yy+yOffset < 673 + 100
-                    //    && xx+xOffset >= 1107 && xx+xOffset < 1107+100)
-                    //    Console.Write("#");
-                    //else
                     Console.Write(v);
                     sum += v;
                 }
---
+++ AdventOfCode/Year2019/Tests/Tester2019_19.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode.Utility;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Year2019.Tests
+{
+    public class Tester2019_19
+    {
+        [Fact]
+        public void FirstProblemMyInput()
+        {
+            Solver2019_19_1.Solve(new FileInputResolver(2019, 19)).Should().Be("173");
+        }
+
+        [Fact]
+        public void SecondProblemMyInput()
+        {
+            Solver2019_19_2.Solve(new FileInputResolver(2019, 19), 600, 1000).Should().Be("6671097");
+        }
+    }
+}
