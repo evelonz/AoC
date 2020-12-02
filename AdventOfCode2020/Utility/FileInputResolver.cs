7fa5c6e89f98f987273f98e685d7aa5dc1309656
Robert Jernberg <robert.jernberg@devcode.se>
2020-12-02 06:27:59 +0100

    Year 2020 project setup

---
+++ AdventOfCode2020/Utility/FileInputResolver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020.Utility
+{
+    internal class FileInputResolver : IInputResolver
+    {
+        private readonly int _day;
+
+        public FileInputResolver(int day)
+        {
+            _day = day;
+        }
+
+        #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+        public IEnumerable<string> AsEnumerable()
+        {
+            using StreamReader reader = AsStream();
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                yield return line;
+            }
+        }
+
+        private StreamReader AsStream()
+        {
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                $"Day{_day}/{_day}.txt");
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+            return new StreamReader(fileStream);
+        }
+    }
+}
---
+++ AdventOfCode2020/Utility/IInputResolver.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020.Utility
+{
+    internal interface IInputResolver
+    {
+        IEnumerable<string> AsEnumerable();
+    }
+}
