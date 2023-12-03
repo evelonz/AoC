366dbd5a659cfc6b098be394ccdf3146608832e0
Robert Jernberg <robert.jernberg@devcode.se>
2023-12-03 01:18:36 +0100

    Refactor 2023 day 2

--- AdventOfCodeFs/2023/SolveDay2.fs
+++ AdventOfCodeFs/2023/SolveDay2.fs
@@ -14,10 +14,31 @@ let exampleData1 = seq {
         yield "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
     }
 
-let parseGameId (row: string) =
-    let gameId = (((row.Split ':')[0]).Split ' ')[1]
-    let (_, id) = System.Int32.TryParse(gameId:string)
-    id
+// A union type did not turn out to be such a hit.
+// At least not with the values stored in it.
+// As the group by became rather messy anyway. Could have used a (marble, count:int) tuple.
+type Marble =
+    | Red of int 
+    | Green of int
+    | Blue of int
+    | Unknown of int
+
+// Function or memeber?
+// should a hand and game be a type or not?
+type Marble with
+    member this.isInvalid =
+            match this with
+            | Red x when x > 12 -> true
+            | Green x when x > 13 -> true
+            | Blue x when x > 14 -> true
+            | _ -> false
+
+    member this.getValue =
+            match this with
+            | Red x -> x
+            | Green x -> x
+            | Blue x -> x
+            | _ -> -1
 
 let parseGame (row: string) = 
     let mutable allHands = []
@@ -28,60 +49,63 @@ let parseGame (row: string) =
         for marbel in marbels do
             let (_, count) = System.Int32.TryParse((marbel.Split ' ')[1])
             let colour = (marbel.Split ' ')[2]
-            hand <- List.append hand [(count, colour)]
+            let marble =
+                match colour with
+                | "red" -> Red count
+                | "green" -> Green count
+                | "blue" -> Blue count
+                | _ -> Unknown count
+            hand <- List.append hand [marble]
         allHands <- List.append allHands [(hand)]
     allHands
 
-let handNotPossible hand =
-    match hand with
-    | (count, color) when color = "red" && count > 12 -> true
-    | (count, color) when color = "green" && count > 13 -> true
-    | (count, color) when color = "blue" && count > 14 -> true
-    | _ -> false
+let rec invalidHand (hand: Marble list) =
+    hand |> List.exists (fun x -> x.isInvalid)
 
-let rec PossibleGame hands =
-    match hands with
-    | [] -> true
-    | head::tail ->
-        let notPossibleGame = handNotPossible head
-        if notPossibleGame
-            then false
-            else PossibleGame tail
+let parseGameId (row: string) =
+    let gameId = (((row.Split ':')[0]).Split ' ')[1]
+    let (_, id) = System.Int32.TryParse(gameId:string)
+    id
 
-let PossibleGames gameRow =
+let getGameScore gameRow =
     let game = parseGame gameRow
-    let mutable validgame = true
-    for hands in game do
-        let result = PossibleGame hands
-        validgame <- if validgame && not result then false else validgame
-    if validgame then parseGameId gameRow else 0
+    let gameIsValid = 
+        game
+        |> List.exists (fun x -> invalidHand x)
+        |> not
+    if gameIsValid then parseGameId gameRow else 0
 
 let solve input =
     input
-    |> Seq.map PossibleGames
+    |> Seq.map getGameScore
     |> Seq.sum
 
-let getMaxOfColour input =
-    let colour = fst input
-    let marbles = snd input
-    let max = List.maxBy (fun x -> fst x) marbles |> fst
-    //printf "%A, %A" colour max
-    max
+// Part 2
+let getMaxOfColour (singleColorMarbleList: Marble list) =
+    let max = List.maxBy (fun x -> x) singleColorMarbleList
+    max.getValue
 
 let getPowerOfCubes groupedInputOfHand =
     groupedInputOfHand
-    |> List.map getMaxOfColour
-    |> List.fold (fun total n -> total * n) 1
+    |> Seq.map getMaxOfColour
+    |> Seq.fold (fun total n -> total * n) 1
+
+let gamesGroupedByColor allGames =
+    allGames
+    |> Seq.map (fun x ->
+        x
+        |> parseGame
+        |> List.concat // Flattens the games into a single list
+        |> List.groupBy (fun e -> e.GetType()) // This is a bit of a bummer, that out of the box it groups on type and value.
+        |> List.map snd
+    )
 
 let solve2 input =
     input
-    |> List.map (fun x ->
-            parseGame x
-            |> List.concat
-            |> List.groupBy (fun x -> snd x)    
-        )
-    |> List.map getPowerOfCubes
-    |> List.sum
+    |> gamesGroupedByColor
+    |> Seq.map getPowerOfCubes
+    |> Seq.sum
+
 
 let [<Fact>] ``part one examples``() =
     solve exampleData1 |> should equal 8
@@ -100,19 +124,19 @@ let [<Fact>] ``parse game id``() =
    parseGameId "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" |> should equal 1
 
 let [<Fact>] ``parse game``() =
-    parseGame "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" |> should equal [[(3, "blue"); (4, "red")]; [(1, "red"); (2, "green"); (6, "blue")];[(2, "green")]]
+    parseGame "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" |> should equal [[Blue 3; Red 4]; [Red 1; Green 2; Blue 6]; [Green 2]]
 
-let [<Fact>] ``impossible marble``() =
-    handNotPossible (13, "red") |> should equal true
+let [<Fact>] ``invalid hand``() =
+    invalidHand [Blue 3; Red 14] |> should equal true
 
-let [<Fact>] ``possible hand``() =
-    PossibleGame [(3, "blue"); (4, "red")] |> should equal true
+let [<Fact>] ``valid hand``() =
+    invalidHand [Blue 3; Red 1] |> should equal false
 
-let [<Fact>] ``possible game``() =
-    PossibleGames "Game 40: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 10 red" |> should equal 0
+let [<Fact>] ``get games score``() =
+    getGameScore "Game 40: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 14 blue, 10 red" |> should equal 40
 
 let [<Fact>] ``max of colour``() =
-    getMaxOfColour ("blue", [(3, "blue"); (6, "blue")]) |> should equal 6
+    getMaxOfColour [Blue 3; Blue 6] |> should equal 6
 
 let [<Fact>] ``power of cubes``() =
-    getPowerOfCubes [("blue", [(3, "blue"); (6, "blue")]); ("red", [(4, "red"); (1, "red")]);("green", [(2, "green"); (2, "green")])] |> should equal 48
+    getPowerOfCubes [[Blue 3; Blue 6]; [Red 4; Red 1]; [Green 2; Green 2]] |> should equal 48
